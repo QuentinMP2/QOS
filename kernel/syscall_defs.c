77714868460384fa61612936a594891528511821
Quentin <pointeauq@bde.enseeiht.fr>
2025-02-22 13:24:46 +0100

    Feat screen

--- kernel/syscall_defs.c
+++ kernel/syscall_defs.c
@@ -4,6 +4,6 @@ fn_ptr syscall_table[NB_SYSCALL];
 
 void add_syscall(int num, fn_ptr function) {
   if (num < NB_SYSCALL) {
-    syscall_table[num]= function;
+    syscall_table[num] = function;
   }
 }
